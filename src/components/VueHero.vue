3ac7cf151fbe32575be50aea588095d56ce70a6f
Jaiden DeChon <jaiden.dechon@gmail.com>
2018-11-05 18:33:51 -0600

    Updated hero to new design

--- src/components/VueHero.vue
+++ src/components/VueHero.vue
@@ -1,168 +1,96 @@
 
 <template>
-    <div id="hero-image">
-        <div id="center">
-            <div id="module">
-                <p class="stack">Les Paul</p>
-                <button>shop now</button>
-            </div>
-        </div>
+  <div id="hero-container">
+    <div id="hero-text">
+      <h1 id="hero-big">yosemite</h1>
+      <p id="hero-small">open-source e-commerce by Jaiden DeChon</p>
     </div>
-
-    <!-- Thanks to Chris Eisenbraun for text stacking idea -->
-    <!-- https://codepen.io/chriseisenbraun -->
-
+  </div>
 </template>
 
 <script>
-import categories from "../assets/data.json";
-var data = JSON.parse(JSON.stringify(categories));
-
-export default {
-  name: "VueHero",
-  data: function() {
-    return {
-      data
-    };
-  },
-
-  mounted: function() {
-    $(document).ready(function() {
-      $(".stack").lettering();
-    });
-  }
-};
 </script>
 
 <style scoped>
-#hero-image {
-  background-image: url(https://goo.gl/u6QqAh);
+@import url("https://fonts.googleapis.com/css?family=Advent+Pro|Raleway");
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: normal;
+}
+
+#hero-container {
+  background-image: url(https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f0a5c81634f9e5cebb8062f676e6d74a&auto=format&fit=crop&w=1950&q=80);
   background-size: cover;
-  background-position: center;
-
-  width: 90vw;
-  height: 80vh;
-
-  margin: 0 auto 5vh;
-
+  background-position: center center;
   border-radius: 10px;
-}
-
-#center {
-  box-sizing: border-box;
-
-  margin: 0;
-  padding: 0;
-
-  height: 100%;
-  width: 100%;
-
+  width: 90vw;
+  margin: 0 auto 35px auto;
   display: flex;
-  justify-content: flex-end;
+}
+
+#hero-text {
+  width: 100%;
+  height: 100%;
+  margin: 0 10vw;
+  text-shadow: 0 0 20px black;
+  display: flex;
   align-items: center;
+  justify-content: center;
+  flex-direction: column;
 }
 
-p {
-  margin: 0;
+#hero-big {
+  font-family: Advent Pro, sans-serif;
+  font-size: 80px;
+  letter-spacing: -5px;
+  color: white;
+  margin: 10px 0;
 }
 
-#module {
-  /* Stacking, shadows should be fixed */
-  box-sizing: border-box;
-
-  width: 600px;
-
-  margin: 0 100px -70px 200px;
-  padding: 0;
-
-  float: right;
-
-  text-align: center;
-}
-
-.stack {
-  /*   border: solid red; */
-
-  font-family: "Righteous", serif;
-  /*   text-transform: uppercase; */
-  /*   text-align: left; */
-  font-size: 15em;
-  line-height: 0.7em;
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-
-  color: silver;
-}
-
-#module button {
-  border: 3px solid silver;
-  outline: none;
-
-  margin: 80px auto;
-
-  width: 60%;
-  height: 50px;
-
+#hero-small {
+  color: white;
   font-family: "Raleway", sans-serif;
-  text-transform: uppercase;
-  font-size: 170%;
-  color: silver;
-
-  background-color: #333;
-
-  transition: all 0.3s;
-
-  cursor: pointer;
-
-  z-index: 100 !important;
-
-  border-radius: 2px;
+  text-align: center;
+  margin: 10px 0;
 }
 
-#module button:hover {
-  border: 3px solid #333;
-  color: #333;
-  background-color: silver;
-}
-
-@media screen and (max-width: 960px) {
-  #hero-image {
-    background-image: url(https://goo.gl/gTC46P);
-    margin-top: 40px;
-  }
-
-  #center {
-    justify-content: center;
-  }
-
-  #module {
-    margin: 0 0 -20% 0;
-
-    font-size: 70%;
-
-    width: 90%;
-  }
-
-  .stack {
-    font-size: 20vw;
-    color: #fff;
-  }
-
-  button {
-    color: #fff;
-    border: 3px solid #fff;
-  }
-
-  button:hover {
-    background-color: #fff;
-    color: #000;
-    border: 3px solid #000;
+@media (min-width: 280px) {
+  #hero-container {
+    height: 650px;
   }
 }
+@media (min-width: 568px) {
+  #hero-big {
+    font-size: 100px;
+  }
+}
+@media (min-width: 750px) {
+  #hero-big {
+    font-size: 125px;
+    letter-spacing: -8px;
+  }
+}
+@media (min-width: 801px) {
+  #hero-big {
+    font-size: 150px;
+  }
+}
+@media (min-width: 1000px) {
+  #hero-big {
+    font-size: 170px;
+  }
 
-@media screen and (min-width: 1000px) {
-  #hero-image {
-    height: 780px
+  #hero-container {
+    height: 780px;
+  }
+}
+@media (min-width: 1920px) {
+  #hero-big {
+    font-size: 200px;
   }
 }
 </style>
