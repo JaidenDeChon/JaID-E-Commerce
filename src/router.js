a156d37bbd87e1c3f0b8b7838938977d2570b97a
Jaiden DeChon <jaiden.dechon@gmail.com>
2018-11-06 04:40:19 -0500

    Moved item view on route to its own path instead of child

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Category from "./views/Category.vue";
+import Item from "./views/Item.vue";
 
 Vue.use(Router);
 
@@ -22,14 +24,19 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("./views/Category.vue"),
-      children: [
-        {
-          path: ":itemTitle",
-          name: "item",
-          component: () => import("./views/Item.vue")
-        }
-      ]
+      component: Category
+      // children: [
+      //   {
+      //     path: "/:itemTitle",
+      //     name: "item",
+      //     component: Item
+      //   }
+      // ]
+    },
+    {
+      path: "/:categoryName/:itemTitle",
+      name: "item",
+      component: Item
     }
   ]
 });
