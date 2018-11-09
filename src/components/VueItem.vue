09e48ef33dfe8ca0f73dc875f7ef6ad2b08f4648
Jaiden DeChon <jaiden.dechon@gmail.com>
2018-11-08 20:13:10 -0600

    Added ability to view/customize items for shopper

--- src/components/VueItem.vue
+++ src/components/VueItem.vue
@@ -1,221 +1,208 @@
+// To do
+// 
+// Change more than just 1 image when color is chosen
+// Allow image changes when more than 1 image is held in the selected color
+
 <template>
   <div id="thing">
-    <!-- The v-for below doesn't access anything, which is why the {{}} isn't displaying anything; there's no match! -->
     <div v-for="category in this.$store.state.catelogue.categories" v-if="category.name === $route.params.categoryName" :key="category">
-      <div v-for="item in category.items" v-if="item.title === $route.params.itemTitle" :key="item.title" class="temp">
-        <!-- item.whatever can now be addressed -->
-        <strong>(Gallery and other shit will go here)</strong>
-        <br>
-        {{ item.title }}
-        <br>
-        {{ item.price }}
-        <br>
-        {{ item.description }}
-      </div>
-    </div>
+      <div v-for="item in category.items" v-if="item.title === $route.params.itemTitle" :key="item.title">
 
+        <div class="carousel-wrapper">
 
-    <!-- <div class="carousel-wrapper">
-      <div class="carousel-container">
-        <div class="image-container"><img id="main-image"/></div>
-        <div class="image-container"><img src="https://goo.gl/tKbfjY"/></div>
-        <div class="image-container"><img src="https://goo.gl/ETvyyE"/></div>
-        <div class="image-container"><img src="https://goo.gl/jg6DfY"/></div>
-      </div>
-      <div class="panel-container">
-        <div class="panel">
-          <h1 class="product-name">Adidas Ultra Boost</h1>
-          <h3 class="product-price">$199.99</h3>
-          <div class="colors">
-            <button class="button button-green selected" data-pic="https://goo.gl/h9eTGE">
-              <h2>Olive</h2>
-            </button>
-            <button class="button button-red" data-pic="https://goo.gl/7kc4pf"> 
-              <h2>Pastel</h2>
-            </button>
-            <button class="button" data-pic="https://goo.gl/2c4Fd5"> 
-              <h2>Black</h2>
-            </button>
+          <div class="carousel-container">
+            <div class="image-container">
+              <img :src="item.mainImage" id="main-image"/>
+            </div>
+
+            <div v-for="image in item.images" v-if="item.title === $route.params.itemTitle" :key="image" class="image-container">
+              <img :src="image"/>
+            </div>
           </div>
-          <div class="sizes">
-            <button class="button">
-              <h2>SM</h2>
-            </button>
-            <button class="button">
-              <h2>MD</h2>
-            </button>
-            <button class="button">
-              <h2>LG</h2>
-            </button>
-            <button class="button">
-              <h2>XL</h2>
-            </button>
+
+          <div class="panel-container">
+            <div class="panel">
+              <h1 class="product-name">{{ item.title }}</h1>
+              <h3 class="product-price">{{ item.price }}</h3>
+
+              <div v-if="item.colors.length > 0" class="colors">
+                <template v-for="color in item.colors">
+                  <button v-for="(key, value) in color" class="button" :data-pic="key" :key="key">
+                    <h2>{{ value }}</h2>
+                  </button>
+                </template>
+              </div>
+              <div v-else="" class="colors">
+                <h3>There are no color options for this item. </h3>
+              </div>
+
+              <div v-if="item.sizes.length > 0" class="sizes">
+                <template v-for="size in item.sizes">
+                  <button class="button" :key="size">
+                    <h2>{{ size }}</h2>
+                  </button>
+                </template>
+              </div>
+              <div v-else="" class="sizes">
+                <h3>There are no size options for this item. </h3>
+              </div>
+
+              <form class="quantity" id="counter-form" action="#" method="POST">
+                <button class="qty-button qtyminus" field="quantity">-</button>
+                <input class="qty" tpye="number" name="quantity" value="1"/>
+                <button class="qty-button qtyplus" field="quantity">+</button>
+              </form>
+              <div class="add-to-cart-container">
+                <button class="button add-to-cart-button"> 
+                  <h2>Add to Cart</h2>
+                </button>
+              </div>
+            </div>
           </div>
-          <form class="quantity" id="counter-form" action="#" method="POST">
-            <button class="qty-button qtyminus" field="quantity">-</button>
-            <input class="qty" tpye="number" name="quantity" value="1"/>
-            <button class="qty-button qtyplus" field="quantity">+</button>
-          </form>
-          <div class="add-to-cart-container">
-            <button class="button add-to-cart-button"> 
-              <h2>Add to Cart</h2>
-            </button>
+        </div>
+        <div class="description-wrapper">
+          <div class="description-header">
+            <h1>Time to walk on clouds. </h1>
+          </div>
+          <div class="description-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
           </div>
         </div>
       </div>
     </div>
-    <div class="description-wrapper">
-      <div class="description-header">
-        <h1>Time to walk on clouds. </h1>
-      </div>
-      <div class="description-body">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-      </div>
-    </div> -->
-
-
-
   </div>
 </template>
 
-// <script>
+<script>
+import jQuery from "jquery";
+import Slick from "vue-slick";
+// import "node_modules/slick-carousel/slick/slick.css";
+
 export default {
-  name: "VueItem"
+  name: "VueItem",
+  components: { Slick },
+  mounted: function() {
+    $(document).ready(function() {
+      $(".carousel-container").slick({
+        // setting-name: setting-value
+        arrows: true,
+        dots: true,
+        cssEase: "ease-out",
+        speed: 200,
+        useTransform: true,
+        fade: true
+      });
+
+      // let initialPic = $(".colors")
+      //   .find(".selected")
+      //   .data("pic");
+      // $("#main-image").attr("src", initialPic);
+    });
+
+    // Button Selecting
+    // When .button is clicked,
+    $(".button").click(function() {
+      // Find the other .buttons within this parent and remove .active
+      $(this)
+        .parent()
+        .find(".button")
+        .not(this)
+        .removeClass("selected");
+
+      // If the button clicked is the cart button...
+      if ($(this).hasClass("add-to-cart-button")) {
+        // If the item is not in cart...
+        if (!$(this).hasClass("in-cart")) {
+          // (Add to cart);
+
+          // Change button text to show it's in the cart
+          $(this)
+            .find("h2")
+            .html("Remove from Cart");
+
+          // Give class 'in-cart' to set status of item
+          $(this).addClass("in-cart");
+
+          // otherwise...
+        } else if ($(this).hasClass("in-cart")) {
+          // (Remove from cart);
+
+          // Change button text to show it's not in the cart
+          $(this)
+            .find("h2")
+            .html("Add to Cart");
+
+          // Remove 'in-cart' class to set status of item
+          $(this).removeClass("in-cart");
+        }
+
+        // Otherwise, if it's a color button...
+      } else if (
+        $(this)
+          .parent()
+          .hasClass("colors")
+      ) {
+        // Get the button's custom data-pic attribute
+        let pic = $(this).data("pic");
+
+        // Change the main image source to the one from data-pic
+        $("#main-image").attr("src", pic);
+      }
+
+      // Toggle .active for the button clicked
+      $(this).toggleClass("selected");
+    });
+
+    // Quantity Counter
+    jQuery(document).ready(function() {
+      // This button will increment the value
+      $(".qtyplus").click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+
+        // Get the field name
+        let fieldName = $(this).attr("field");
+
+        // Get its current value
+        var currentVal = parseInt($("input[name=" + fieldName + "]").val());
+
+        // If is not undefined
+        if (!isNaN(currentVal)) {
+          // Increment
+          $("input[name=" + fieldName + "]").val(currentVal + 1);
+        } else {
+          // Otherwise put a 0 there
+          $("input[name=" + fieldName + "]").val(0);
+        }
+      });
+
+      // This button will decrement the value till 0
+      $(".qtyminus").click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+
+        // Get the field name
+        let fieldName = $(this).attr("field");
+
+        // Get its current value
+        var currentVal = parseInt($("input[name=" + fieldName + "]").val());
+
+        // If it isn't undefined or its greater than 0
+        if (!isNaN(currentVal) && currentVal > 0) {
+          // Decrement one
+          $("input[name=" + fieldName + "]").val(currentVal - 1);
+        } else {
+          // Otherwise put a 0 there
+          $("input[name=" + fieldName + "]").val(0);
+        }
+      });
+    });
+  }
 };
-// export default {
-//   name: "VueItem",
-//   mounted: function() {
-//     $(document).ready(function() {
-//       $(".carousel-container").slick({
-//         // setting-name: setting-value
-//         arrows: true,
-//         dots: true,
-//         cssEase: "ease-out",
-//         speed: 200,
-//         useTransform: true,
-//         fade: true
-//       });
-
-//       let initialPic = $(".colors")
-//         .find(".selected")
-//         .data("pic");
-//       $("#main-image").attr("src", initialPic);
-//     });
-
-//     // Button Selecting
-//     // When .button is clicked,
-//     $(".button").click(function() {
-//       // Find the other .buttons within this parent and remove .active
-//       $(this)
-//         .parent()
-//         .find(".button")
-//         .not(this)
-//         .removeClass("selected");
-
-//       // If the button clicked is the cart button...
-//       if ($(this).hasClass("add-to-cart-button")) {
-//         // If the item is not in cart...
-//         if (!$(this).hasClass("in-cart")) {
-//           // (Add to cart);
-
-//           // Change button text to show it's in the cart
-//           $(this)
-//             .find("h2")
-//             .html("Remove from Cart");
-
-//           // Give class 'in-cart' to set status of item
-//           $(this).addClass("in-cart");
-
-//           // otherwise...
-//         } else if ($(this).hasClass("in-cart")) {
-//           // (Remove from cart);
-
-//           // Change button text to show it's not in the cart
-//           $(this)
-//             .find("h2")
-//             .html("Add to Cart");
-
-//           // Remove 'in-cart' class to set status of item
-//           $(this).removeClass("in-cart");
-//         }
-
-//         // Otherwise, if it's a color button...
-//       } else if (
-//         $(this)
-//           .parent()
-//           .hasClass("colors")
-//       ) {
-//         // Get the button's custom data-pic attribute
-//         let pic = $(this).data("pic");
-
-//         // Change the main image source to the one from data-pic
-//         $("#main-image").attr("src", pic);
-//       }
-
-//       // Toggle .active for the button clicked
-//       $(this).toggleClass("selected");
-//     });
-
-//     // Quantity Counter
-//     jQuery(document).ready(function() {
-//       // This button will increment the value
-//       $(".qtyplus").click(function(e) {
-//         // Stop acting like a button
-//         e.preventDefault();
-
-//         // Get the field name
-//         fieldName = $(this).attr("field");
-
-//         // Get its current value
-//         var currentVal = parseInt($("input[name=" + fieldName + "]").val());
-
-//         // If is not undefined
-//         if (!isNaN(currentVal)) {
-//           // Increment
-//           $("input[name=" + fieldName + "]").val(currentVal + 1);
-//         } else {
-//           // Otherwise put a 0 there
-//           $("input[name=" + fieldName + "]").val(0);
-//         }
-//       });
-
-//       // This button will decrement the value till 0
-//       $(".qtyminus").click(function(e) {
-//         // Stop acting like a button
-//         e.preventDefault();
-
-//         // Get the field name
-//         fieldName = $(this).attr("field");
-
-//         // Get its current value
-//         var currentVal = parseInt($("input[name=" + fieldName + "]").val());
-
-//         // If it isn't undefined or its greater than 0
-//         if (!isNaN(currentVal) && currentVal > 0) {
-//           // Decrement one
-//           $("input[name=" + fieldName + "]").val(currentVal - 1);
-//         } else {
-//           // Otherwise put a 0 there
-//           $("input[name=" + fieldName + "]").val(0);
-//         }
-//       });
-//     });
-//   }
 </script>
 
 <style>
-.temp {
-  background: lightgray;
-  color: #333;
-  text-align: center;
-  font-family: "Raleway";
-  font-size: 20px;
-  width: 90vw;
-  margin: 35px auto;
-  padding: 30px 0;
-  border-radius: 10px;
-}
-/* @import url("https://fonts.googleapis.com/css?family=Raleway|Montserrat");
+@import url("https://fonts.googleapis.com/css?family=Raleway|Montserrat");
 
 body {
   margin: 0;
@@ -340,6 +327,18 @@ body {
     grid-template-columns: repeat(4, 20fr);
   }
 
+  .colors h3,
+  .sizes h3 {
+    color: #333;
+    font-family: "Montserrat";
+    text-align: center;
+    min-width: 302px;
+    min-width: 302px;
+    /* border: solid red; */
+    padding: 0;
+    margin: 0;
+  }
+
   .quantity {
     grid-template-columns: 60px 100px 60px;
   }
@@ -360,11 +359,14 @@ body {
     box-sizing: border-box;
     border-radius: 5px;
     font-family: "Raleway", sans-serif;
-    font-size: 150%;
+    font-size: 15px;
     text-transform: uppercase;
     color: #333;
     background: #e8e8e8;
     transition: all 0.2s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     cursor: pointer;
   }
   .button.selected {
@@ -467,28 +469,6 @@ body {
     background: #333;
     color: #999;
   }
-
-  .button-fieg {
-    color: #fff;
-    font-weight: bold;
-    background: #efefef;
-  }
-  .button-fieg h2 {
-    color: #333;
-    background: url("https://goo.gl/uR4Y9E");
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-  }
-  .button-fieg.selected {
-    background: url("https://goo.gl/uR4Y9E");
-    color: #fff;
-  }
-  .button-fieg.selected h2 {
-    color: #fff;
-    background: #fff;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-  }
 }
 @media (min-width: 0px) and (min-width: 1000px) {
   .button-fieg:hover {
@@ -552,5 +532,107 @@ body {
   .image-container img {
     height: 70vh;
   }
-} */
+}
+
+.slick-slider {
+  position: relative;
+
+  display: block;
+  box-sizing: border-box;
+
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  -webkit-touch-callout: none;
+  -khtml-user-select: none;
+  -ms-touch-action: pan-y;
+  touch-action: pan-y;
+  -webkit-tap-highlight-color: transparent;
+}
+
+.slick-list {
+  position: relative;
+
+  display: block;
+  overflow: hidden;
+
+  margin: 0;
+  padding: 0;
+}
+.slick-list:focus {
+  outline: none;
+}
+.slick-list.dragging {
+  cursor: pointer;
+  cursor: hand;
+}
+
+.slick-slider .slick-track,
+.slick-slider .slick-list {
+  -webkit-transform: translate3d(0, 0, 0);
+  -moz-transform: translate3d(0, 0, 0);
+  -ms-transform: translate3d(0, 0, 0);
+  -o-transform: translate3d(0, 0, 0);
+  transform: translate3d(0, 0, 0);
+}
+
+.slick-track {
+  position: relative;
+  top: 0;
+  left: 0;
+
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.slick-track:before,
+.slick-track:after {
+  display: table;
+
+  content: "";
+}
+.slick-track:after {
+  clear: both;
+}
+.slick-loading .slick-track {
+  visibility: hidden;
+}
+
+.slick-slide {
+  display: none;
+  float: left;
+
+  height: 100%;
+  min-height: 1px;
+}
+[dir="rtl"] .slick-slide {
+  float: right;
+}
+.slick-slide img {
+  display: block;
+}
+.slick-slide.slick-loading img {
+  display: none;
+}
+.slick-slide.dragging img {
+  pointer-events: none;
+}
+.slick-initialized .slick-slide {
+  display: block;
+}
+.slick-loading .slick-slide {
+  visibility: hidden;
+}
+.slick-vertical .slick-slide {
+  display: block;
+
+  height: auto;
+
+  border: 1px solid transparent;
+}
+.slick-arrow.slick-hidden {
+  display: none;
+}
 </style>
