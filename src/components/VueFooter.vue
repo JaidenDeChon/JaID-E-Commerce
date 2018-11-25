788ac593d4ccaf4ca383300b6c1f9900e23ac47e
Jaiden DeChon <jaiden.dechon@gmail.com>
2018-11-25 05:53:00 -0500

    Modified links

--- src/components/VueFooter.vue
+++ src/components/VueFooter.vue
@@ -7,9 +7,9 @@
             <div id="navigate">
                 <h3>Navigate</h3>
                 <router-link target="_self" to="/">Home</router-link><br>
-                <router-link target="_self" to="/faq">FAQ</router-link><br>
-                <router-link target="_self" to="/legal">Legal</router-link><br>
+                <router-link target="_self" to="/about">About</router-link><br>
                 <router-link target="_self" to="/our-story">Our Story</router-link><br>
+                <router-link target="_self" to="/legal">Legal</router-link><br>
                 <router-link target="_self" to="/site-map">Site Map</router-link><br>
             </div>
 
