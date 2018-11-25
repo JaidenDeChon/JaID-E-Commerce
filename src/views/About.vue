a930d2e89b614fd1c04b22f863d6b310aa6f7b7e
Jaiden DeChon <jaiden.dechon@gmail.com>
2018-11-25 05:53:26 -0500

    Added components to About view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Route is working</h1>
+    <vue-about-hero />
+    <vue-quote />
+    <vue-slider />
+    <vue-goals />
+    <vue-team />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import VueAboutHero from "@/components/about/VueAboutHero.vue";
+import VueQuote from "@/components/about/VueQuote.vue";
+import VueSlider from "@/components/about/VueSlider.vue";
+import VueGoals from "@/components/about/VueGoals.vue";
+import VueTeam from "@/components/about/VueTeam.vue";
+import VueFooter from "@/components/VueFooter.vue";
+
+export default {
+  name: "About",
+  components: {
+    VueAboutHero,
+    VueQuote,
+    VueSlider,
+    VueGoals,
+    VueTeam,
+    VueFooter
+  }
+};
+</script>
